422ecdd57e27850175305ec79e2274987df759d4
i-made-that <rogerthomasson@gmail.com>
2014-05-08 22:11:30 -0700

    Change showCode to click.  Add simpleCode property.  Convert existing function widgets accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -10,329 +10,6 @@ var	genericForm = document.getElementById('genericForm'),
 		textField = document.getElementById('textField');
 
 
-
-
-// Object literal template, use for adding new functions
-//
-// {
-// 	title: '',
-// 	description: '',
-// 	placeholder: 'Type me anything',
-// 	primaryFunction:,
-// 	helperFunction: undefined
-// },
-
-///////////////////////////////////////////////////////////////////////////////////
-// Big Coderbyte Object Array
-//
-//
-var arrayOfObjects = [
-{
-	title: 'abCheck',
-	description: 'Reports whether the letters \'a\' and \'b\' occur in a string, and are also separated by 3 places.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-	function abCheck(str) {
-		'use strict';
-		if (str.match(/[aA]...[bB]/g) || str.match(/[bB]...[aA]/g)) {
-			return 'Well yes, they are!';
-		} return 'No such luck...';
-	},
-	helperFunction: undefined
-},
-
-{
-	title: 'alphabetSoup',
-	description: 'Alphabetizes a string, numbers first',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function alphabetSoup(str) {
-			'use strict';
-			return str.split('').sort().join('');
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'dashInsert',
-	description: 'Inserts a dash between any two adjacent odd numbers in a string.<br>0 doesn\'t count as odd',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function dashInsert (num) {
-			'use strict';
-			var str = num.toString();
-			var newStr = '';
-			for (var i = 0; i < str.length; i++) {
-				if ((str.charAt(i) % 2 === 1) && (str.charAt(i+1) % 2 === 1)) {
-					newStr += str.charAt(i) + '-';
-				} else newStr += str.charAt(i);
-			}
-			return newStr;
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'firstFactorial',
-	description: 'Takes number, returns factorial',
-	placeholder: 'Type me a number',
-	primaryFunction:
-	function firstFactorial (num) {
-		'use strict';
-		if (isNaN(num)) {
-			return '<span class="red">I need a number, please!</span>';
-		} else if (parseInt(num) === 0) {
-			return num + '!' + ' is equal to 1';
-		} else if (num < 0) {
-			return '<span class="red">I need a POSITIVE number, please!</span>';
-		} else {
-			var result = num;
-			for (var i = num; i > 1; i--) {
-				result *= (i -1);
-			}
-			return num + '!' + ' is equal to ' + Math.round(result);
-		}
-	},
-	helperFunction: undefined
-},
-
-{
-	title: 'firstReverse',
-	description: 'This function reverses a string',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function firstReverse(str) {
-			'use strict';
-			return 'Reversed: ' + str.split('').reverse().join('');
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'letterCapitalize',
-	description: 'Capitalize the first letter of each word.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function letterCapitalize (str) {
-			'use strict';
-			var newStr = str.charAt(0).toUpperCase();
-			for (var i = 1; i < str.length; i++) {
-				if (str.charAt(i - 1) === ' ') {
-					newStr += str.charAt(i).toUpperCase();
-				} else newStr += str.charAt(i);
-			}
-		return newStr;
-	},
-	helperFunction: undefined
-},
-
-{
-	title: 'letterChanges',
-	description: 'A function that takes a string, and replaces every letter in the string with the letter that follows in the alphabet.<br>Then, it capitalizes all vowels.<br>Numbers stay the same.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function letterChanges(str) {
-		'use strict';
-		str.toLowerCase();
-		var newStr = '',
-				len = str.length;
-		for (var i = 0; i < len; i++) {
-			if ((/z/i).test(str.charAt(i))) {
-				newStr += 'a';
-			} else if ((/[a-y]/i).test(str.charAt(i))) {
-				newStr += String.fromCharCode((str.charCodeAt(i) + 1));
-			} else newStr += str.charAt(i);
-		}
-		return this.helperFunction(newStr);
-	},
-	helperFunction:
-		function capitalizeVowels(str) {
-		'use strict';
-		var newStr = '',
-				len = str.length;
-		for (var i = 0; i < len; i++) {
-			if ((/[aeiou]/i).test(str.charAt(i))) {
-				newStr += str.charAt(i).toUpperCase();
-			} else newStr += str.charAt(i);
-		}
-		return newStr;
-	}
-},
-
-{
-	title: 'longestWord',
-	description: 'Returns the longest word in a sentence.<br>If there is more than one longest word with the same length, it returns the first.<br>Symbols and punctuation are <em>not</em> accounted for.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function longestWord(sen) {
-			'use strict';
-			var noPunc = sen.replace(/[^\w\s]|_/g, '');
-			var wordArray = noPunc.split(' ');
-			var longWord = wordArray[0];
-			for( var i=1; i < wordArray.length; i++ ){
-				if (wordArray[i].length > longWord.length){
-					longWord = wordArray[i];
-				}
-			}
-			return 'The longest word is: ' + longWord;
-},
-	helperFunction: undefined
-},
-
-{
-	title: 'numberAddition',
-	description: 'Adds all numbers found in a string (with or without letters & symbols).<br>Adjacent numbers matter (AKA 88sb3 = 91; 8b8b3 = 19)',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function numberAddition (str) {
-			'use strict';
-			var total = 0;
-			var num = 0;
-			for (var i = 0; i < str.length; i++) {
-				if (str.charAt(i) === ' ') {
-					total += parseInt(num);
-					num = 0;
-				}
-				else if (!isNaN(str.charAt(i))) {
-					num += str.charAt(i);
-				}
-				else {
-					total += parseInt(num);
-					num = 0;
-				}
-			}
-			total += parseInt(num);
-			return total;
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'palindrome',
-	description: 'Determines whether or not a string is a Palindrome.<br>Ignores numbers and punctuation.<br>Spaces don\'t count.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-	function palindrome(str) {
-		'use strict';
-    var newStr = '';
-	
-
-		var removeSpaces = function(str){
-			var noSpaceStr = '';
-			for (var i = 0; i < str.length; i++){
-				if (str.charAt(i) !== ' '){
-					noSpaceStr += str.charAt(i);
-				}
-			}
-			return noSpaceStr;
-		};
-		for (var i = removeSpaces(str).length; i >= 0; i--){
-			newStr = newStr + removeSpaces(str).charAt(i);
-		}
-		if (removeSpaces(str) === newStr){
-			return 'Success! Palindrome!';
-		} else return 'Sorry, not a palindrome...';
-	},
-	helperFunction: undefined
-},
-
-{
-	title: 'simpleAdding',
-	description: 'Adds all the numbers up from 1 to n (n being your input)',
-	placeholder: 'Type me a number',
-	primaryFunction:
-		function simpleAdding(num) {
-			'use strict';
-			if (isNaN(num)) {
-				return '<span class="red">I need a number, please!</span>';
-			} else if (parseInt(num) === 0) {
-				return num + '!' + ' is equal to 1';
-			} else if (num > 10000000) {
-				return '<span class="red">A number that big might kill your browser!</span>';
-			} else if (num < 0) {
-				return '<span class="red">I need a POSITIVE number, please!</span>';
-			} else if (num % 1 !== 0) {
-				return '<span class="red">No decimals, please!</span>';
-			} else if (parseInt(num) === 1) {
-				return 1;
-			} else if (parseInt(num) === 2) {
-				return '1 + 2 = 3';
-			} else
-			{
-				var temp = 0;
-				for(var i = 1; i <= num; i++){
-					temp = temp + i;
-				}
-				return '1 + 2 + 3 + (...) = ' + temp;
-			}
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'simpleSymbols',
-	description: 'Determines if a string is an acceptable sequence.<br>\'Acceptable\' means that any letter must be surrounded by a \'+\' symbol',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function simpleSymbols(str) {
-			'use strict';
-			var isIt = false;
-			for (var i = 0; i < str.length; i++) {
-				if (/[a-zA-Z]/.test(str.charAt(i))) {
-					if (str.charAt(i + 1) === '+' && str.charAt(i - 1) === '+') {
-						isIt = true;
-					} else return '<span class="red">NOT</span>  acceptable!';
-				}
-			}
-			if (isIt) {
-				return 'Acceptable!';
-			}
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'swapCase',
-	description: 'Swaps the upper/lower case of each letter, depending on their original case.<br>Numbers and symbols stay the same.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-	function swapCase(str) {
-		'use strict';
-		var newStr = '';
-		for (var i = 0; i < str.length; i++)
-			if (str.charAt(i) == str.charAt(i).toUpperCase()) {
-				newStr = newStr + str.charAt(i).toLowerCase();
-			} else if (str.charAt(i) == str.charAt(i).toLowerCase()) {
-				newStr = newStr + str.charAt(i).toUpperCase();
-			}
-			return newStr;
-		},
-	helperFunction: undefined
-},
-
-{
-	title: 'vowelCount',
-	description: 'This function counts the number of vowels in a sentence or word.',
-	placeholder: 'Type me anything',
-	primaryFunction:
-		function vowelCount(str) {
-			'use strict';
-			var matchArray = str.match(/[aeiou]/gi);
-			if (str === '') {
-				return '';
-			} else if (matchArray === null) {
-				return '0 vowels total';
-			} else return matchArray.length + ' vowels total in ' + '"' + regexpColor(str, /[aeiou]/gi) + '"' ;
-		},
-	helperFunction: undefined
-}
-];
-//
-//
-//
-// End Big CoderByte Object Array
-////////////////////////////////////////////////////////////////////////////
-
 // Some self-explanatory show/hide functions
 function showElement (element) {
 	'use strict';
@@ -423,6 +100,7 @@ document.getElementById('buttons').addEventListener('click', function(e) {
 		oldButton = currentButton;
 		expanded = true;
 		hideElement(result);
+			hideElement(theCode);
 		genericForm.reset();
 		textField.focus();
 		currentButton.className = 'btn btn-primary';
@@ -438,6 +116,7 @@ document.getElementById('buttons').addEventListener('click', function(e) {
 		hideElement(functionContainer);
 		hideElement(showCode);
 		hideElement(result);
+			hideElement(theCode);
 		genericForm.reset();
 		currentButton.className = 'btn';
 		oldButton.className = 'btn';
@@ -450,6 +129,7 @@ document.getElementById('buttons').addEventListener('click', function(e) {
 		currentButton.className = 'btn btn-primary';
 		populateForm(arrayOfObjects, index);
 		hideElement(result);
+		hideElement(theCode);
 		showElement(showCode);
 		genericForm.reset();
 	}
@@ -478,17 +158,17 @@ document.getElementById('submitButton').addEventListener('click', function(e) {
 
 
 // These buttons show and hide the code for the related function.  Grabbed via 'i' from the array object and pretty printed via whiteSpace function
-document.getElementById('showCode').addEventListener('mouseover', function() {
+document.getElementById('showCode').addEventListener('click', function() {
 	'use strict';
-	var stringCode = arrayOfObjects[i].primaryFunction.toString();
+	var stringCode = arrayOfObjects[i].simpleCode.toString();
 	theCode.innerHTML = prettyCode(stringCode);
 	showElement(theCode);
 }, false);
 
-document.getElementById('showCode').addEventListener('mouseout', function() {
-	'use strict';
-	hideElement(theCode);
-}, false);
+// document.getElementById('showCode').addEventListener('mouseout', function() {
+// 	'use strict';
+// 	hideElement(theCode);
+// }, false);
 
 
 
